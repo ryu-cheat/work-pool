b980fc1675476ae094f24bc0c52da62454993de9
ryujaewan <jw.ryu@plingo.io>
2019-07-22 18:15:19 +0900

    add progress, work key, work remove, work onFinish, clear

--- Pool.js
+++ Pool.js
@@ -1,17 +1,65 @@
 
 function F(maxWorks){
-    const works = [];
-    const workings = [];
+    let works = [];
+    let worksKeys = { }
+    let worksOnFinish = { }
+    let workings = [];
 
-    let finishCallback = null;
+    let workLength = 0;
+    let finishCallback = () => {};
+    let progressCallback = () => {};
 
     function Pool () { }
-    Pool.prototype.work = function (fn = function (next=()=>{}) {}) {
+    Pool.prototype.work = function (fn = function (next=()=>{}) {}, options) {
+        if (!options) options = {};
+        if (options.key == null) {
+            options.key = Math.random() + '';
+        }
+        if (worksKeys[options.key]) return { remove:()=>{}, onFinish:()=>{ return {remove:()=>{}} } };
+        worksKeys[options.key] = fn;
+
+        const remove = () => {
+            let worksLength = works.length;
+            works = works.filter(w => w !== fn);
+            
+            if (worksLength != works.length) {
+                -- workLength;
+            }
+            try{ delete worksKeys[options.key]; }catch(e){}
+            try{ delete worksOnFinish[options.key]; }catch(e){}
+        }
+        const onFinish = (fn) => {
+            worksOnFinish[options.key] = fn
+            return { remove }
+        }
+
+        ++workLength;
+
         works.push(fn);
         doWork();
+        
+        return { remove, onFinish }
     };
     Pool.prototype.finish = function (callback = function () {}) {
         finishCallback = callback;
+        return this;
+    };
+    Pool.prototype.clear = function () {
+        workLength = 0;
+        works = [];
+        workings = [];
+        worksKeys = { }
+        worksOnFinish = { }
+        return this;
+    };
+    Pool.prototype.progress = function (callback = function ({ remainder, progressing, now, max }) {}) {
+        progressCallback = () =>  callback && callback({
+            remainder: works.length,
+            progressing: workings.length,
+            now: workLength - (workings.length + works.length),
+            max: workLength,
+        });
+        return this;
     };
 
     function doWork () {
@@ -21,15 +69,25 @@ function F(maxWorks){
                 const next = () => {
                     let workIndex = workings.indexOf(work);
                     workings.splice(workIndex, 1);
+                    progressCallback()
+
                     if (works.length == 0 && workings.length == 0) {
-                        !!finishCallback && typeof finishCallback === 'function' && finishCallback();
+                        finishCallback && finishCallback();
                     }else{
                         doWork();
+                    }
+
+                    for (let key in worksKeys) {
+                        if (worksKeys[key] === work) {
+                            worksOnFinish[key] && worksOnFinish[key]();
+                            break;
+                        }
                     }
                 };
                 workings.push(work);
                 work(next);
             }
+            progressCallback()
         }
     }
     
--- example.js
+++ example.js
@@ -2,13 +2,20 @@ const Pool = require('./Pool');
 
 const P1 = Pool(3);
 
+
+P1.progress(function ({ progressing, max, now, remainder }) {
+    console.log(progressing, remainder, now, max)
+})
 for (let i = 1 ; i <= 11; i ++) {
-    P1.work(function(next){
+    let a = P1.work(function(next){
         setTimeout(function(){
             console.log( new Date().toLocaleTimeString(), i);
             next();
         }, 1000);
+    }, { key: i }).onFinish(()=>{
+        console.log(i)
     })
+    if (i == 11)a.remove()
 }
 P1.finish(function () {
     console.log('FINISH');
